257fd5db0f2ed2b8f9540fda90d763e6d48dc124
Marcelo Castelo <marcelo.cbo2000@gmail.com>
2024-01-25 19:24:30 -0300

    teste

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def web_page():
-    return render_template("index.html")
+    return render_template("")
 
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')
+    app.run()
